7bda73faffe450664b377f9fe1734c9ded1c3740
ALEXEY <alexsem2ev@gmail.com>
2026-09-13 21:50:57 +0700

    the HeapSortTest class created

--- Task_1_1_1/src/test/java/ru/nsu/asemenychev/Task_1_1_1/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/asemenychev/Task_1_1_1/HeapSortTest.java
@@ -6,5 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HeapSortTest {
 
+    @Test
+    void sort0() {
+        int[] array = {};
+        HeapSort.sort(array);
+        assertArrayEquals(new int[]{}, array);
+    }
 
+    @Test
+    void sort1() {
+        int[] array = {1, 2, 2, 3, 5, 7};
+        HeapSort.sort(array);
+        assertArrayEquals(new int[]{1, 2, 2, 3, 5, 7}, array);
+    }
+
+    @Test
+    void sort2() {
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        HeapSort.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, array);
+    }
 }
