c89517f4ad6d572866de37b02757f378b970db50
ALEXEY <alexsem2ev@gmail.com>
2026-09-14 00:42:31 +0700

    the HeapSortComplexityTest class created again

--- Task_1_1_1/src/test/java/ru/nsu/asemenychev/Task_1_1_1/HeapSortComplexityTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/asemenychev/Task_1_1_1/HeapSortComplexityTest.java
@@ -1,21 +1,36 @@
 package ru.nsu.asemenychev.Task_1_1_1;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-public class HeapSortComplexity {
+
+class HeapSortComplexity {
+
+    @Test
     void time() {
         int[] sizes = {10000, 100000, 1000000, 10000000};
 
         System.out.println();
-        System.out.println("%-14d time (ms)");
+        System.out.println("n\t\t\ttime (ms)");
         for (int n : sizes) {
             int[] data = randomArray(n);
+            int[] expected_data = data.clone();
+            Arrays.sort(expected_data);
+
             long t0 = System.nanoTime();
             HeapSort.sort(data);
             long dt = System.nanoTime() - t0;
-            System.out.printf("%-14d %.3f%n", n, dt / 1000000.0);
+
+            assertArrayEquals(data, expected_data);
+
+            System.out.printf("%-11d %.3f%n", n, dt / 1000000.0);
         }
+
+        System.out.printf("time(10*n) ~ 13*time(n) за счет асимптотики nlogn%n");
     }
 
     static int[] randomArray(int n) {
--- Task_1_1_1/src/test/java/ru/nsu/asemenychev/Task_1_1_1/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/asemenychev/Task_1_1_1/SortTest.java
@@ -7,21 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class SortTest {
 
     @Test
-    void sort0() {
+    void sortEmpty() {
         int[] array = {};
         HeapSort.sort(array);
         assertArrayEquals(new int[]{}, array);
     }
 
     @Test
-    void sort1() {
+    void withOnlyOneElement() {
+        int[] array = {1};
+        HeapSort.sort(array);
+        assertArrayEquals(new int[]{1}, array);
+    }
+
+    @Test
+    void sortWithoutChangesWithoutDuplicates() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        HeapSort.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, array);
+    }
+
+    @Test
+    void sortWithoutChangesWithDuplicates() {
         int[] array = {1, 2, 2, 3, 5, 7};
         HeapSort.sort(array);
         assertArrayEquals(new int[]{1, 2, 2, 3, 5, 7}, array);
     }
 
     @Test
-    void sort2() {
+    void sortRevert() {
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        HeapSort.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, array);
+    }
+
+    @Test
+    void sort() {
         int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
         HeapSort.sort(array);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, array);
