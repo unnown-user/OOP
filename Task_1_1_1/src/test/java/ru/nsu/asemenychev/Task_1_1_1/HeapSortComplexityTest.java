5618354f69ba5abaf0462a1b50bc871897e6d15e
ALEXEY <alexsem2ev@gmail.com>
2026-09-13 22:23:56 +0700

    the HeapSortComplexityTest class created

---
+++ Task_1_1_1/src/test/java/ru/nsu/asemenychev/Task_1_1_1/HeapSortComplexityTest.java
@@ -0,0 +1,32 @@
+package ru.nsu.asemenychev.Task_1_1_1;
+
+import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+public class HeapSortComplexityTest {
+
+    @Test
+    void time() {
+        int[] sizes = {10000, 100000, 1000000, 10000000};
+
+        System.out.println();
+        System.out.println("%-14d time (ms)");
+        for (int n : sizes) {
+            int[] data = randomArray(n);
+            long t0 = System.nanoTime();
+            HeapSort.sort(data);
+            long dt = System.nanoTime() - t0;
+            System.out.printf("%-14d %.3f%n", n, dt / 1000000.0);
+        }
+    }
+
+    static int[] randomArray(int n) {
+        Random r = new Random(100);
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = r.nextInt();
+        }
+
+        return a;
+    }
+}
