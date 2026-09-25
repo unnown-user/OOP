95b78f9060ad7dd190e6a92695785e1d8e59d3cc
Alexey Semenychev <a.semenychev@g.nsu.ru>
2026-09-25 20:32:28 +0700

    add Dealer and Player classes

--- Task_1_1_2/src/main/java/ru/nsu/asemenychev/Task_1_1_2/Dealer.java
+++ Task_1_1_2/src/main/java/ru/nsu/asemenychev/Task_1_1_2/Dealer.java
@@ -1,4 +1,11 @@
 package ru.nsu.asemenychev.Task_1_1_2;
 
-public class Dealer {
+public class Dealer extends Player {
+    public Dealer() {
+        super("Дилер");
+    }
+
+    public boolean shouldHit() {
+        return getHand().total() < 17;
+    }
 }
--- Task_1_1_2/src/main/java/ru/nsu/asemenychev/Task_1_1_2/Player.java
+++ Task_1_1_2/src/main/java/ru/nsu/asemenychev/Task_1_1_2/Player.java
@@ -1,4 +1,26 @@
 package ru.nsu.asemenychev.Task_1_1_2;
 
 public class Player {
+    private final String name;
+    private final Hand hand = new Hand();
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public void clearHand() {
+        hand.clear();
+    }
 }
