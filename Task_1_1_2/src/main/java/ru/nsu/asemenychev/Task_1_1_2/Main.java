1731bef6ec9502bf48319df7435d78a0967ae7e4
Alexey Semenychev <a.semenychev@g.nsu.ru>
2026-09-25 20:42:14 +0700

    updated the Main class

--- Task_1_1_2/src/main/java/ru/nsu/asemenychev/Task_1_1_2/Main.java
+++ Task_1_1_2/src/main/java/ru/nsu/asemenychev/Task_1_1_2/Main.java
@@ -1,2 +1,10 @@
 package ru.nsu.asemenychev.Task_1_1_2;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // new BlackjackGame(scanner).play();
+    }
+}
